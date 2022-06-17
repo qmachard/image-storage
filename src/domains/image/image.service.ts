caf40ea9a655b5718350fae16c561959d0fbaec0
Quentin Machard <quentin.machard@gmail.com>
2022-06-17 15:51:36 +0200

    ✨ Generate filename from mimetype

--- src/domains/image/image.service.ts
+++ src/domains/image/image.service.ts
@@ -1,7 +1,34 @@
-export function getFilepath(filename: string): string {
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+
+export function getFilepath(filename: string, createIfNotExists: boolean = true): string {
     const a = filename.substring(0, 1);
     const b = filename.substring(1, 2);
     const c = filename.substring(2, 3);
 
-    return `${process.env.IMAGE_STORAGE}/${a}/${b}/${c}/${filename}`;
+    const filepath = `${process.env.IMAGE_STORAGE}/${a}/${b}/${c}`;
+
+    if (!fs.existsSync(filepath) && createIfNotExists) {
+        fs.mkdirSync(filepath, { recursive: true });
+    }
+
+    return filepath;
+}
+
+const mimeTypesMapping: Record<string, { extension: string }> = {
+    'image/gif': { extension: 'gif' },
+    'image/png': { extension: 'png' },
+    'image/jpeg': { extension: 'jpeg' },
+    'image/bmp': { extension: 'bmp' },
+    'image/webp': { extension: 'webp' },
+};
+
+export function generateFilename(mimetype: string = 'image/jpeg'): string {
+    const { extension } = mimeTypesMapping[mimetype];
+
+    if (!extension) {
+        throw new Error(`Unknown mimetype "${mimetype}".`);
+    }
+
+    return `${uuidv4()}.${extension}`;
 }
