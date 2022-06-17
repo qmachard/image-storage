39453d72000ee240923c68c106258e2de5365a19
Quentin Machard <quentin.machard@gmail.com>
2022-06-17 15:51:53 +0200

    ✨ Enable to upload image

--- src/domains/image/image.controller.ts
+++ src/domains/image/image.controller.ts
@@ -3,7 +3,9 @@ import fs from 'fs';
 
 import { NotFoundException } from '~/utils/exceptions';
 
-import { getFilepath } from '~/domains/image/image.service';
+import { uploadHandler } from '~/middlewares/upload.handler';
+
+import { generateFilename, getFilepath } from '~/domains/image/image.service';
 
 const ImageController = Router();
 
@@ -12,13 +14,40 @@ ImageController.get('/:filename', (req, res) => {
 
     const filepath = getFilepath(filename);
 
-    if (!fs.existsSync(filepath)) {
+    if (!fs.existsSync(`${filepath}/${filename}`)) {
         throw new NotFoundException(`Image not found.`);
     }
 
     return res
         .status(200)
-        .sendFile(filepath);
+        .sendFile(`${filepath}/${filename}`);
 });
+
+ImageController.post('/', uploadHandler.single('image'), async (req, res) => {
+    if (!req.file) {
+        return res
+            .status(400)
+            .json({
+                error: 'You must provide image file.',
+            });
+    }
+
+    const { path, mimetype } = req.file;
+
+    const filename = generateFilename(mimetype);
+    const filepath = getFilepath(filename);
+
+    // Copy file to destination
+    fs.copyFileSync(path, `${filepath}/${filename}`);
+
+    // Remove file from source
+    fs.rmSync(path);
+
+    return res
+        .status(201)
+        .json({
+            url: `/images/${filename}`,
+        });
+})
 
 export { ImageController };
